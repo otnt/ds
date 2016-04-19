1babad01093964fe41bbc956ae6b4b4e67f8c291
pufan jiang <jiangpufan@gmail.com>
2016-04-19 21:54:01 +0000

    fix fetch bug

--- main/petGag.go
+++ main/petGag.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"fmt"
-=======
 	"github.com/otnt/ds/webService"
 	//"github.com/otnt/ds/replication"
->>>>>>> 1345e4781de4a6cf0e1dec162bc250490e00228e
 	ch "github.com/otnt/ds/consistentHashing"
 	"github.com/otnt/ds/infra"
 	"github.com/otnt/ds/node"
-	"github.com/otnt/ds/replication"
-	"github.com/otnt/ds/webService"
+	//"github.com/otnt/ds/replication"
 	"os"
 	"strconv"
-<<<<<<< HEAD
-	"time"
-=======
 	"github.com/otnt/ds/message"
->>>>>>> 1345e4781de4a6cf0e1dec162bc250490e00228e
+	"time"
 )
 
 func main() {
@@ -32,13 +25,8 @@ func main() {
 
 	//init consistent hashing
 	ring := ch.NewRing()
-<<<<<<< HEAD
-	for _, n := range nodes.Servers {
-		nn := node.Node(n)
-=======
 	for _,n:= range infra.NodeIndexMap{
 		nn := node.Node(*n)
->>>>>>> 1345e4781de4a6cf0e1dec162bc250490e00228e
 		ring.AddSync(&nn)
 	}
 
@@ -61,19 +49,6 @@ func messageDispatcher() {
 		case newMessage := <-infra.ReceivedBuffer:
 			messageKind := message.GetKind(&newMessage)
 
-<<<<<<< HEAD
-		if messageKind == "forward" { /* At the primary */
-			replication.UpdateSelfDB(&newMessage)
-			replication.AskNodesToUpdate(&newMessage)
-			go replication.WaitForAcks()
-			replication.RespondToClient()
-
-		} else if messageKind == "forward" { /* At the secondary */
-			replication.UpdateSelfDB(&newMessage)
-			replication.SendAcks(&newMessage)
-		} else if messageKind == "acknowledgement" { /* Acks processing at the primary */
-			replication.ProcessAcks()
-=======
 			if messageKind == "replication" {
 				//replication.NameOfFunction(&newMessage)
 			} else if messageKind == webService.KIND_FORWARD {
@@ -88,7 +63,6 @@ func messageDispatcher() {
 
 		case <-time.After(time.Millisecond * 1):
 			continue
->>>>>>> 1345e4781de4a6cf0e1dec162bc250490e00228e
 		}
 	}
 }
